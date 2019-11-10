7e5d4ec5c8adbf878cb52baecbf42cab51d45845
Christian E. Hopps <chopps@gmail.com>
2019-11-10 08:55:50 -0500

    Update the generic yang makefile

--- yang.mk
+++ yang.mk
@@ -1,15 +1,37 @@
 BASE := $(shell sed -e '/^\#+RFC_NAME:/!d;s/\#+RFC_NAME: *\(.*\)/\1/' $(ORG))
 VERSION := $(shell sed -e '/^\#+RFC_VERSION:/!d;s/\#+RFC_VERSION: *\([0-9]*\)/\1/' $(ORG))
-VBASE := publish/$(BASE)-$(VERSION)
-LBASE := publish/$(BASE)-latest
-EMACSCMD := emacs -Q --batch --eval '(setq org-confirm-babel-evaluate nil)' -l ./ox-rfc.el
+NEXT_VERSION := $(shell printf "$$(($(VERSION) + 1))")
+PBASE := publish/$(BASE)-$(VERSION)
+VBASE := draft/$(BASE)-$(VERSION)
+LBASE := draft/$(BASE)-latest
+
+# If you have docker you can avoid having to install anything by leaving this.
+#export DOCKRUN ?= docker run --network=host -v $$(pwd):/work labn/org-rfc
+
+EMACSCMD := $(DOCKRUN) emacs -Q --batch --debug-init --eval '(setq org-confirm-babel-evaluate nil)' -l ./ox-rfc.el
+# EMACSCMD := emacs -Q --batch --eval '(setq org-confirm-babel-evaluate nil)' -l ./ox-rfc.el
 
 all: $(LBASE).xml $(LBASE).txt $(LBASE).html # $(LBASE).pdf
 
 clean:
 	rm -f $(BASE).xml $(BASE)-*.{txt,html,pdf} $(LBASE).*
 
+.PHONY: publish
+publish: $(VBASE).xml $(VBASE).txt $(VBASE).html
+	if [ -f $(PBASE).xml ]; then echo "$(PBASE).xml already present, increment version?"; exit 1; fi
+	cp $(VBASE).xml $(VBASE).txt $(VBASE).html publish
+	git add $(PBASE).xml $(PBASE).txt $(PBASE).html
+	sed -i -e 's/\#+RFC_VERSION: *\([0-9]*\)/\#+RFC_VERSION: $(NEXT_VERSION)/' $(ORG)
+
+publish-update:
+	cp $(VBASE).xml $(VBASE).txt $(VBASE).html publish
+	git add $(PBASE).xml $(PBASE).txt $(PBASE).html
+
+draft:
+	mkdir -p draft
+
 $(VBASE).xml: $(ORG) ox-rfc.el
+	mkdir -p draft
 	$(EMACSCMD) $< -f ox-rfc-export-to-xml
 	mv $(BASE).xml $@
 
